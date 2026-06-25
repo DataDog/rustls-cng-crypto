d1ad916b8e33c58b7642b47d044e38c6598f1c58
thieman <travis.thieman@datadoghq.com>
2026-06-25 10:19:05 -0400

    test(tls12): import max fragment length in regression test

--- src/tls12.rs
+++ src/tls12.rs
@@ -352,8 +352,8 @@ mod test {
     use rustls::{ContentType, Error, ProtocolVersion, SupportedCipherSuite};
 
     use super::{
-        TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-        TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+        MAX_FRAGMENT_LEN, TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+        TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
     };
 
     #[test]
